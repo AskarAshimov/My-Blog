93703be3792f6ee6312eeaf7d5e20e2253a712fb
Askar <maskascar@gmail.com>
2022-06-20 21:25:33 +0600

    init:start project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
 
       <header className="w3-container w3-center w3-padding-32">
-        <h1><b>Уникальные детки</b></h1>
+        <h1><b>MY BLOG</b></h1>
         <p>Welcome to the blog of <span className="w3-tag">unknown</span></p>
       </header>
 
